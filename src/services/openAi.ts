431f0e9bd1c5bc8e2a6c9f3d661b54869bd946d1
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2023-07-28 00:24:27 -0400

    documentation chat working

--- src/services/openAi.ts
+++ src/services/openAi.ts
@@ -16,10 +16,26 @@ const generateImage = async (title: string) => {
   });
 
   const imageUrl = response.data.data[0].url as string;
-  return imageUrl
+  return imageUrl;
+};
+
+const completion = async () => {
+  const response: any = await openAi.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "Hello world" },
+    ],
+  });
+
+  const rawTextAnswer = response.data.choices[0].message.content;
+
+  console.log({ rawTextAnswer });
 };
 
 export const generateSEO = async (title: string) => {
-  const imageUrl = await generateImage(title);
+  //const imageUrl = await generateImage(title);
+  const imageUrl = "Temporary - not run out my credits =S";
+  const hastags = await completion();
   return { imageUrl };
 };
