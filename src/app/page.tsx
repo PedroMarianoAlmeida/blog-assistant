9df2a8206fd563b190daa626894f70f084550efd
PedroMarianoAlmeida <pedroalmeidaprogrammer@gmail.com>
2023-07-31 15:11:51 -0400

    add sign in and sign up buttons on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import Link from "next/link";
+import Button from "@mui/material/Button";
+
 export default function Home() {
   return (
     <main>
       <h1>Landing Page</h1>
+      <Link href="/sign-in">
+        <Button variant="contained">Sign In</Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button variant="contained">Sign Up</Button>
+      </Link>
     </main>
   );
 }
