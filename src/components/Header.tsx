4db651814abe34206aef29b4855b4cbc75455889
PedroMarianoAlmeida <pedroalmeidaprogrammer@gmail.com>
2023-07-31 15:22:35 -0400

    add clerk UserButton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export const Header = () => (
   <AppBar position="static">
-    <Typography component="div" align="center" py={2}>
-      Blog Assistant
-    </Typography>
+    <Box display="flex" alignItems="center" justifyContent="space-between" px={2}>
+      <Typography component="div" align="center" py={2}>
+        Blog Assistant
+      </Typography>
+      <UserButton afterSignOutUrl="/" />
+    </Box>
   </AppBar>
 );
