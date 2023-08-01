3a45fc5dac9ea1d68999a1accff71c8d05134793
PedroMarianoAlmeida <pedroalmeidaprogrammer@gmail.com>
2023-08-01 00:58:53 -0400

    fix eslint error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,19 +7,20 @@ import { SeoResponseData } from "@/types/openAiResponse";
 import { GeneratedSEO } from "@/components/GeneratedSEO";
 
 const Dashboard = () => {
-    const [seoResponseData, setSeoResponseData] = useState<SeoResponseData>({
-        loading: false,
-        imageUrl: "",
-        hashtags: [],
-        error: false,
-      });
-    
-      return (
-        <main>       
-          <ArticleTitleAssistantForm setSeoResponseData={setSeoResponseData} />
-          <GeneratedSEO seoResponseData={seoResponseData} />
-        </main>
-      );
+  const [seoResponseData, setSeoResponseData] = useState<SeoResponseData>({
+    loading: false,
+    imageUrl: "",
+    hashtags: [],
+    error: "",
+    usage: 0
+  });
+
+  return (
+    <main>
+      <ArticleTitleAssistantForm setSeoResponseData={setSeoResponseData} />
+      <GeneratedSEO seoResponseData={seoResponseData} />
+    </main>
+  );
 };
 
 export default Dashboard;
