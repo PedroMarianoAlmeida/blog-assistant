2620923b38fff0d4fda2ea9f3ee7dd5c021a9a24
PedroMarianoAlmeida <pedroalmeidaprogrammer@gmail.com>
2023-08-01 00:23:12 -0400

    create a landing page
    https://github.com/mui/material-ui/blob/v5.14.2/docs/data/material/getting-started/templates/album/Album.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,54 @@
-"use client";
-
 import Link from "next/link";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
 
 export default function Home() {
   return (
     <main>
-      <h1>Landing Page</h1>
-      <Link href="/sign-in">
-        <Button variant="contained">Sign In</Button>
-      </Link>
-      <Link href="/sign-up">
-        <Button variant="contained">Sign Up</Button>
-      </Link>
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Blog Assistant
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            Add a images and generate hashtags for your articles in a blink of
+            eye
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Link href="/sign-in">
+              <Button variant="contained">Sign In</Button>
+            </Link>
+            <Link href="/sign-up">
+              <Button variant="outlined">Sign Up</Button>
+            </Link>
+          </Stack>
+        </Container>
+      </Box>
     </main>
   );
 }
